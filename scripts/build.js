fddcc4b07b8dc93499850d770c33e17ef3e8ebae
ty <towyuan@outlook.com>
2021-07-28 18:48:31 +0800

    2.8.1

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-commonjs, global-require */
-const execa = require('execa');
-
-const build = async (buildDir, tsTemp) => {
-  await execa('tsc -p tsconfig.json', { shell: true });
-  await execa(`npx babel ./${tsTemp} -d ${buildDir}`, { shell: true });
-};
-
-module.exports = build;
--- scripts/publish.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-commonjs, global-require */
-const execa = require('execa');
-
-const publish = (dir, npmTag) =>
-  execa(`npm publish --registry https://registry.npmjs.com --access public ${dir} --tag ${npmTag}`, {shell: true});
-
-module.exports = publish;
