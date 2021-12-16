dec6546ddf9c2fd7cf4b3d57d6d7177667e7dd9e
ty <towyuan@outlook.com>
2021-12-16 22:15:40 +0800

    feat: add isBuffer without importing node Buffer

---
+++ src/is/__tests__/buffer.ts
@@ -0,0 +1,23 @@
+import isBuffer from '../buffer';
+
+describe('utils/is/boolean', () => {
+  it('test', () => {
+    expect(isBuffer(Buffer.from([4]))).toBe(true);
+    expect(isBuffer(Buffer.alloc(4))).toBe(true);
+    expect(isBuffer(Buffer.allocUnsafeSlow(100))).toBe(true);
+    expect(isBuffer(undefined)).toBe(false);
+    expect(isBuffer(null)).toBe(false);
+    expect(isBuffer('')).toBe(false);
+    expect(isBuffer(true)).toBe(false);
+    expect(isBuffer(false)).toBe(false);
+    expect(isBuffer(0)).toBe(false);
+    expect(isBuffer(1)).toBe(false);
+    expect(isBuffer(1.0)).toBe(false);
+    expect(isBuffer('string')).toBe(false);
+    expect(isBuffer({})).toBe(false);
+    expect(isBuffer([])).toBe(false);
+    expect(isBuffer(function foo () {})).toBe(false);
+    expect(isBuffer({ isBuffer: null })).toBe(false);
+    expect(isBuffer({ isBuffer: function () { throw new Error() } })).toBe(false);
+  });
+});
---
+++ src/is/buffer.ts
@@ -0,0 +1,27 @@
+/**
+ * Determine if an object is a Buffer (including the browserify Buffer).
+ *
+ * This function is from https://github.com/feross/is-buffer
+ *
+ * @param {*} test
+ * @returns {boolean} Returns `true` if `value` is a Buffer, else `false`.
+ * @example
+ *
+ * isBuffer(new Buffer(4)) // =>true
+ * isBuffer(Buffer.alloc(4)) // => true
+ *
+ * isBuffer(undefined) // =>false
+ * isBuffer(null) // =>false
+ * isBuffer('') // =>false
+ * isBuffer(true) // =>false
+ * isBuffer(false) // =>false
+ * isBuffer(0) // =>false
+ * isBuffer(1) // =>false
+ * isBuffer(1.0) // =>false
+ * isBuffer('string') // =>false
+ * isBuffer({}) // =>false
+ * isBuffer(function foo () {}) // =>false
+ */
+export const isBuffer = (test: any): test is Buffer => test != null && test.constructor != null && typeof test.constructor.isBuffer === 'function' && test.constructor.isBuffer(test);
+
+export default isBuffer;
