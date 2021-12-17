99a4be1c52eb042b0cdfcbdb744333c696daa0a1
ty <towyuan@outlook.com>
2021-12-17 17:43:46 +0800

    feat: add fromCallback

---
+++ src/promise/__tests__/fromCallback.ts
@@ -0,0 +1,58 @@
+import repeat from '../../array/repeat';
+import fromCallback from '../fromCallback';
+
+function greeter(options?: {
+  error?: any;
+  argsCount?: number;
+}) {
+  const throws = options?.error;
+  const argsCount = options?.argsCount ?? 0;
+  return (cb: (error: any, ...args: any[]) => void) => {
+    if (throws) {
+      cb(throws);
+    } else if (argsCount === 0) {
+      cb(null);
+    } else if (argsCount > 0) {
+      cb(null, ...repeat(argsCount, 'hi'));
+    }
+  }
+}
+
+function greetWithThis(cb: (error: any, ...args: any[]) => void) {
+  cb(null, this);
+}
+
+describe('fromCallback', () => {
+  it('should handle error', function() {
+    return expect(fromCallback(cb => greeter({
+      error: new Error('A sample error')
+    })(cb))).rejects.toThrow('A sample error');
+  });
+
+  it('should handle empty argument', function() {
+    return expect(fromCallback(cb => greeter({
+      argsCount: 0,
+    })(cb))).resolves.toBeUndefined();
+  });
+
+  it('should handle one argument', function() {
+    return expect(fromCallback(cb => greeter({
+      argsCount: 1,
+    })(cb))).resolves.toBe('hi');
+  });
+
+  it('should handle multiple arguments', function() {
+    return expect(fromCallback(cb => greeter({
+      argsCount: 3,
+    })(cb))).resolves.toEqual(['hi', 'hi', 'hi']);
+  });
+
+  it('should work with thisArg', async () => {
+    const obj = {};
+    const result = await fromCallback(function (cb){
+      greetWithThis.call(this, cb)
+    }, obj);
+    expect(result).toBe(obj);
+  });
+
+});
---
+++ src/promise/fromCallback.ts
@@ -0,0 +1,42 @@
+export interface FromCallbackOptions {
+  multiArgs: boolean;
+}
+
+/**
+ * Returns a promise that is resolved by a node style callback function. This is the most fitting way to do on the fly
+ * promisification when libraries don't expose classes for automatic promisification by undefined.
+ *
+ * The resolver function is passed a callback that expects to be called back according to error-first node conventions.
+ *
+ * Setting multiArgs to true means the resulting promise will always fulfill with an array of the callback's success
+ * value(s). This is needed because promises only support a single success value while some callback API's have multiple
+ * success value. The default is to ignore all but the first success value of a callback function.
+ *
+ * @param fn
+ * @param thisArg
+ * @example
+ *
+ * const fs = require('fs');
+ *
+ * fromCallback(callback => fs.readFile('filename.txt', callback))
+ *   .then(buffer => buffer.toString().trim());
+ *   // ...
+ *
+ */
+export const fromCallback = (fn: (callback: (err: any, ...args: any[]) => any) => void, thisArg?: any): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    fn.call(thisArg, (err: any, ...args: any[]) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (args.length <= 1) {
+          resolve(args[0]);
+        } else {
+          resolve(args);
+        }
+      }
+    });
+  });
+};
+
+export default fromCallback;
