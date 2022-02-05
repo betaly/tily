bc1b519473b1ea3885ab4654e0277fc0b24cdb03
ty <towyuan@outlook.com>
2022-02-05 11:54:23 +0800

    add: string/count

---
+++ src/string/count.ts
@@ -0,0 +1,20 @@
+/**
+ * Counts how often `character` occurs inside `value`.
+ *
+ * @param value
+ * @param character
+ *
+ * @example
+ *
+ * count('Hello World', 'o'); //=> 2
+ */
+export function count(value: string, character: string): number {
+  let result = 0;
+  const ch = character.charCodeAt(0);
+  for (let i = value.length - 1; i >= 0; i--) {
+    if (value.charCodeAt(i) === ch) {
+      result++;
+    }
+  }
+  return result;
+}
