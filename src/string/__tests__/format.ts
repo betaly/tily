a5a65746669678d42c224e9e9dd6ae28c2c45d01
ty <towyuan@outlook.com>
2022-02-05 11:54:45 +0800

    add: string/format

---
+++ src/string/__tests__/format.ts
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import {format} from '../format';
+
+describe('string/format', function() {
+  test('format', () => {
+    assert.strictEqual(format('Foo Bar'), 'Foo Bar');
+    assert.strictEqual(format('Foo {0} Bar'), 'Foo {0} Bar');
+    assert.strictEqual(format('Foo {0} Bar', 'yes'), 'Foo yes Bar');
+    assert.strictEqual(format('Foo {0} Bar {0}', 'yes'), 'Foo yes Bar yes');
+    assert.strictEqual(format('Foo {0} Bar {1}{2}', 'yes'), 'Foo yes Bar {1}{2}');
+    assert.strictEqual(format('Foo {0} Bar {1}{2}', 'yes', undefined), 'Foo yes Bar undefined{2}');
+    assert.strictEqual(format('Foo {0} Bar {1}{2}', 'yes', 5, false), 'Foo yes Bar 5false');
+    assert.strictEqual(format('Foo {0} Bar. {1}', '(foo)', '.test'), 'Foo (foo) Bar. .test');
+  });
+});
---
+++ src/string/format.ts
@@ -0,0 +1,27 @@
+const _formatRegexp = /{(\d+)}/g;
+
+/**
+ * Helper to produce a string with a variable number of arguments. Insert variable segments
+ * into the string using the {n} notation where N is the index of the argument following the string.
+ *
+ * @param value string to which formatting is applied
+ * @param args replacements for {n}-entries
+ *
+ * @example
+ *
+ * format('Foo {0} Bar {1}{2}', 'yes');             //=> 'Foo yes Bar {1}{2}'
+ * format('Foo {0} Bar {1}{2}', 'yes', undefined);  //=> 'Foo yes Bar undefined{2}'
+ * format('Foo {0} Bar {1}{2}', 'yes', 5, false);   //=> 'Foo yes Bar 5false'
+ *
+ */
+export function format(value: string, ...args: any[]): string {
+  if (args.length === 0) {
+    return value;
+  }
+  return value.replace(_formatRegexp, function (match, group) {
+    const idx = parseInt(group, 10);
+    return isNaN(idx) || idx < 0 || idx >= args.length ?
+      match :
+      args[idx];
+  });
+}
