17aa248c11ac0c6f436efe271cc0de564faf0ec1
ty <towyuan@outlook.com>
2022-02-05 11:54:52 +0800

    add: string/format2

---
+++ src/string/__tests__/format2.ts
@@ -0,0 +1,15 @@
+import assert from 'assert';
+import {format2} from '../format2';
+
+describe('string/format2', function() {
+  test('format2', () => {
+    assert.strictEqual(format2('Foo Bar', {}), 'Foo Bar');
+    assert.strictEqual(format2('Foo {oops} Bar', {}), 'Foo {oops} Bar');
+    assert.strictEqual(format2('Foo {foo} Bar', { foo: 'bar' }), 'Foo bar Bar');
+    assert.strictEqual(format2('Foo {foo} Bar {foo}', { foo: 'bar' }), 'Foo bar Bar bar');
+    assert.strictEqual(format2('Foo {foo} Bar {bar}{boo}', { foo: 'bar' }), 'Foo bar Bar {bar}{boo}');
+    assert.strictEqual(format2('Foo {foo} Bar {bar}{boo}', { foo: 'bar', bar: 'undefined' }), 'Foo bar Bar undefined{boo}');
+    assert.strictEqual(format2('Foo {foo} Bar {bar}{boo}', { foo: 'bar', bar: '5', boo: false }), 'Foo bar Bar 5false');
+    assert.strictEqual(format2('Foo {foo} Bar. {bar}', { foo: '(foo)', bar: '.test' }), 'Foo (foo) Bar. .test');
+  });
+});
---
+++ src/string/format2.ts
@@ -0,0 +1,19 @@
+const _format2Regexp = /{([^}]+)}/g;
+
+/**
+ * Helper to create a string from a template and a string record.
+ * Similar to `format` but with objects instead of positional arguments.
+ *
+ * @param template
+ * @param values
+ *
+ * @example
+ *
+ * format2('Foo {foo} Bar {bar}{boo}', { foo: 'bar' });                       //=> 'Foo bar Bar {bar}{boo}'
+ * format2('Foo {foo} Bar {bar}{boo}', { foo: 'bar', bar: 'undefined' });     //=> 'Foo bar Bar undefined{boo}'
+ * format2('Foo {foo} Bar {bar}{boo}', { foo: 'bar', bar: '5', boo: false }); //=> 'Foo bar Bar 5false'
+ * 
+ */
+export function format2(template: string, values: Record<string, unknown>): string {
+  return template.replace(_format2Regexp, (match, group) => (values[group] ?? match) as string);
+}
