a76d75803048b374e5e81104f3a42105864b4fe0
ty <towyuan@outlook.com>
2022-02-05 11:55:07 +0800

    add: string/ltrim

---
+++ src/string/__tests__/ltrim.ts
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import {ltrim} from '../ltrim';
+
+describe('string/ltrim', function() {
+  test('ltrim', () => {
+    assert.strictEqual(ltrim('foo', 'f'), 'oo');
+    assert.strictEqual(ltrim('foo', 'o'), 'foo');
+    assert.strictEqual(ltrim('http://www.test.com', 'http://'), 'www.test.com');
+    assert.strictEqual(ltrim('/foo/', '/'), 'foo/');
+    assert.strictEqual(ltrim('//foo/', '/'), 'foo/');
+    assert.strictEqual(ltrim('/', ''), '/');
+    assert.strictEqual(ltrim('/', '/'), '');
+    assert.strictEqual(ltrim('///', '/'), '');
+    assert.strictEqual(ltrim('', ''), '');
+    assert.strictEqual(ltrim('', '/'), '');
+  });
+});
---
+++ src/string/ltrim.ts
@@ -0,0 +1,29 @@
+
+/**
+ * Removes all occurrences of needle from the beginning of haystack.
+ *
+ * @param haystack string to trim
+ * @param needle the thing to trim
+ *
+ * @example
+ *
+ * ltrim('abcabcHello', 'abc'); //=> 'Hello'
+ * ltrim('  Hello  ');          //=> 'Hello  '
+ */
+export function ltrim(haystack: string, needle: string = ' '): string {
+  if (!haystack || !needle) {
+    return haystack;
+  }
+
+  const needleLen = needle.length;
+  if (needleLen === 0 || haystack.length === 0) {
+    return haystack;
+  }
+
+  let offset = 0;
+
+  while (haystack.indexOf(needle, offset) === offset) {
+    offset = offset + needleLen;
+  }
+  return haystack.substring(offset);
+}
