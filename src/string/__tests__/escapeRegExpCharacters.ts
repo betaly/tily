f2a6cb022bea6b78051fdfb79e3e566b4b2f0479
ty <towyuan@outlook.com>
2022-02-06 13:22:42 +0800

    chore: remove some less used new imported functions

--- src/string/__tests__/escapeRegExpCharacters.ts
+++
@@ -1,7 +0,0 @@
-import {escapeRegExpCharacters} from '../escapeRegExpCharacters';
-
-describe('string/escapeRegExpCharacters', function() {
-  test('escapeRegExpCharacters', () => {
-    expect(escapeRegExpCharacters('/{([^}]+)}/g')).toEqual('/\\{\\(\\[\\^\\}\\]\\+\\)\\}/g');
-  });
-});
--- src/string/__tests__/isFalsyOrWhitespace.ts
+++
@@ -1,9 +0,0 @@
-import {isFalsyOrWhitespace} from '../isFalsyOrWhitespace';
-
-describe('string/isFalsyOrWhitespace', function() {
-  test('isFalsyOrWhitespace', () => {
-    expect(isFalsyOrWhitespace('')).toBe(true);
-    expect(isFalsyOrWhitespace('  ')).toBe(true);
-    expect(isFalsyOrWhitespace('  hello  ')).toBe(false);
-  })
-});
