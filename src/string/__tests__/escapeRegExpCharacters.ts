8840589f281efe27af048dba5a0cc4979ba6e300
ty <towyuan@outlook.com>
2022-03-16 11:21:17 +0800

    chore: reformat

--- src/string/__tests__/escapeRegExpCharacters.ts
+++ src/string/__tests__/escapeRegExpCharacters.ts
@@ -1,6 +1,6 @@
 import {escapeRegExpCharacters} from '../escapeRegExpCharacters';
 
-describe('string/escapeRegExpCharacters', function() {
+describe('string/escapeRegExpCharacters', function () {
   test('escapeRegExpCharacters', () => {
     expect(escapeRegExpCharacters('/{([^}]+)}/g')).toEqual('/\\{\\(\\[\\^\\}\\]\\+\\)\\}/g');
   });
